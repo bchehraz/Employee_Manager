4cf0b5035999ed044e68fb8381201529dc8ce8e7
Babak Chehraz <bchehraz@csumb.edu>
2017-08-28 13:52:47 -0700

    More boilerplate. Login works now with error and loading.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-    render() {
-        return (
-            <Provider store={createStore()}>
-                <View>
-                    <Text>
-                        Hello!
-                    </Text>
-                </View>
-            </Provider>
-        );
-    }
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAkIWrLQpoGLW6_iaMoS6ResGTgFyZEtMQ',
+      authDomain: 'employee-manager-38d36.firebaseapp.com',
+      databaseURL: 'https://employee-manager-38d36.firebaseio.com',
+      projectId: 'employee-manager-38d36',
+      storageBucket: 'employee-manager-38d36.appspot.com',
+      messagingSenderId: '294836656548'
+  };
+
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+    return (
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    );
+  }
 }
 
 export default App;
