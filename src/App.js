2e8412797056e4252948bc18af93b7b25494d47c
Babak Chehraz <bchehraz@csumb.edu>
2017-08-28 14:16:07 -0700

    added some stuff

--- src/App.js
+++ src/App.js
@@ -5,17 +5,11 @@ import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import Router from './Router';
+import { firebaseConfigInfo } from './firebase';
 
 class App extends Component {
   componentWillMount() {
-    const config = {
-      apiKey: 'AIzaSyAkIWrLQpoGLW6_iaMoS6ResGTgFyZEtMQ',
-      authDomain: 'employee-manager-38d36.firebaseapp.com',
-      databaseURL: 'https://employee-manager-38d36.firebaseio.com',
-      projectId: 'employee-manager-38d36',
-      storageBucket: 'employee-manager-38d36.appspot.com',
-      messagingSenderId: '294836656548'
-  };
+    const config = firebaseConfigInfo;
 
     firebase.initializeApp(config);
   }
